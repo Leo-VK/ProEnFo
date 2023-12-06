34111194f11ec50fa23a56ead3bcd321d5852013
phet1999 <u3009646@connect.hku.hk>
2023-12-06 13:23:32 +0800

    Transformer_varients

--- models/pytorch.py
+++ models/pytorch.py
@@ -16,6 +16,13 @@ from typing import List
 from models.shift import DishTS,RevIN
 from itertools import chain
 
+from fed_former_utils.Embed import DataEmbedding, DataEmbedding_wo_pos
+from fed_former_utils.AutoCorrelation import AutoCorrelation, AutoCorrelationLayer
+from fed_former_utils.FourierCorrelation import FourierBlock, FourierCrossAttention
+from fed_former_utils.MultiWaveletCorrelation import MultiWaveletCross, MultiWaveletTransform
+from fed_former_utils.SelfAttention_Family import FullAttention, ProbAttention
+from fed_former_utils.Autoformer_EncDec import Encoder, Decoder, EncoderLayer, DecoderLayer, my_Layernorm, series_decomp, series_decomp_multi
+
 
 
 class PytorchRegressor(BaseEstimator, RegressorMixin):
@@ -733,3 +740,129 @@ def trend_model(thetas, t, device):
 def linear_space(backcast_length, forecast_length, is_forecast=True):
     horizon = forecast_length if is_forecast else backcast_length
     return np.arange(0, horizon) / horizon
+
+
+class Fedformer(nn.Module):
+    """
+    FEDformer performs the attention mechanism on frequency domain and achieved O(N) complexity
+    """
+    def __init__(self, configs):
+        super(Fedformer, self).__init__()
+        self.version = configs.version
+        self.mode_select = configs.mode_select
+        self.modes = configs.modes
+        self.seq_len = configs.seq_len
+        self.label_len = configs.label_len
+        self.pred_len = configs.pred_len
+        self.output_attention = configs.output_attention
+
+        # Decomp
+        kernel_size = configs.moving_avg
+        if isinstance(kernel_size, list):
+            self.decomp = series_decomp_multi(kernel_size)
+        else:
+            self.decomp = series_decomp(kernel_size)
+
+        # Embedding
+        # The series-wise connection inherently contains the sequential information.
+        # Thus, we can discard the position embedding of transformers.
+        self.enc_embedding = DataEmbedding_wo_pos(configs.enc_in, configs.d_model, configs.embed, configs.freq,
+                                                  configs.dropout)
+        self.dec_embedding = DataEmbedding_wo_pos(configs.dec_in, configs.d_model, configs.embed, configs.freq,
+                                                  configs.dropout)
+
+        if configs.version == 'Wavelets':
+            encoder_self_att = MultiWaveletTransform(ich=configs.d_model, L=configs.L, base=configs.base)
+            decoder_self_att = MultiWaveletTransform(ich=configs.d_model, L=configs.L, base=configs.base)
+            decoder_cross_att = MultiWaveletCross(in_channels=configs.d_model,
+                                                  out_channels=configs.d_model,
+                                                  seq_len_q=self.seq_len // 2 + self.pred_len,
+                                                  seq_len_kv=self.seq_len,
+                                                  modes=configs.modes,
+                                                  ich=configs.d_model,
+                                                  base=configs.base,
+                                                  activation=configs.cross_activation)
+        else:
+            encoder_self_att = FourierBlock(in_channels=configs.d_model,
+                                            out_channels=configs.d_model,
+                                            seq_len=self.seq_len,
+                                            modes=configs.modes,
+                                            mode_select_method=configs.mode_select)
+            decoder_self_att = FourierBlock(in_channels=configs.d_model,
+                                            out_channels=configs.d_model,
+                                            seq_len=self.seq_len//2+self.pred_len,
+                                            modes=configs.modes,
+                                            mode_select_method=configs.mode_select)
+            decoder_cross_att = FourierCrossAttention(in_channels=configs.d_model,
+                                                      out_channels=configs.d_model,
+                                                      seq_len_q=self.seq_len//2+self.pred_len,
+                                                      seq_len_kv=self.seq_len,
+                                                      modes=configs.modes,
+                                                      mode_select_method=configs.mode_select)
+        # Encoder
+        enc_modes = int(min(configs.modes, configs.seq_len//2))
+        dec_modes = int(min(configs.modes, (configs.seq_len//2+configs.pred_len)//2))
+        print('enc_modes: {}, dec_modes: {}'.format(enc_modes, dec_modes))
+
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AutoCorrelationLayer(
+                        encoder_self_att,
+                        configs.d_model, configs.n_heads),
+
+                    configs.d_model,
+                    configs.d_ff,
+                    moving_avg=configs.moving_avg,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=my_Layernorm(configs.d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AutoCorrelationLayer(
+                        decoder_self_att,
+                        configs.d_model, configs.n_heads),
+                    AutoCorrelationLayer(
+                        decoder_cross_att,
+                        configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.c_out,
+                    configs.d_ff,
+                    moving_avg=configs.moving_avg,
+                    dropout=configs.dropout,
+                    activation=configs.activation,
+                )
+                for l in range(configs.d_layers)
+            ],
+            norm_layer=my_Layernorm(configs.d_model),
+            projection=nn.Linear(configs.d_model, configs.c_out, bias=True)
+        )
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        # decomp init
+        mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
+        zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]])  # cuda()
+        seasonal_init, trend_init = self.decomp(x_enc)
+        # decoder input
+        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+        seasonal_init = F.pad(seasonal_init[:, -self.label_len:, :], (0, 0, 0, self.pred_len))
+        # enc
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        # dec
+        dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
+        seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
+                                                 trend=trend_init)
+        # final
+        dec_out = trend_part + seasonal_part
+
+        if self.output_attention:
+            return dec_out[:, -self.pred_len:, :], attns
+        else:
+            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
